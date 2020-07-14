3bc9f29cc2fa04ba2b5648201b1c7bcdbae93a6a
Louai Abdessaied <57537005+Abdessaied@users.noreply.github.com>
2020-07-14 20:42:11 +0100

    Main

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
-const {BrowserWindow,app} = electron;
+const {BrowserWindow,app,Menu,ipcMain} = electron;
 const path = require('path');
+
+require('electron-reload')(__dirname);
 
 var window_width = 1280;
 var window_height = 720;
@@ -10,6 +12,25 @@ app.on('ready',function()
 {
     window = new BrowserWindow({width: window_width,height: window_height,icon: path.join(__dirname,'Icon.png'),webPreferences: {nodeIntegration: true}});
     window.loadURL('file://' + __dirname + '/views/index.html');
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'File',
+            submenu: [
+                {label: 'Create Node...',click: function()
+            {
+                window.webContents.send('create');
+            }},
+                {label: 'Run',click: function()
+            {
+                window.webContents.send('run');
+            }},
+                {label:'Exit'}
+            ]
+        }
+    ]);
+
+    Menu.setApplicationMenu(menu); 
 });
 
 app.on('close',function()
